bbf325a5f9578506f982a8a2e2418273330b77a1
Sullivan Atatri <satatri@inouit.com>
2018-11-21 08:08:29 +0100

    [add] theme, wireframe and part2

---
+++ my-theme/footer.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>&copy; Tous droits réservés</p>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ my-theme/functions.php
@@ -0,0 +1,7 @@
+<?php
+register_nav_menus( array(
+  'main' => 'Menu principal',
+  'footer' => 'Menu footer',
+) );
+
+?>
